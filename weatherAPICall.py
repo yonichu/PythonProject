4c69a54b17bf8e0effd501bc4e58532217eb4f0a
yoni storaro <73361803+yonichu@users.noreply.github.com>
2022-02-20 18:17:49 +0000

    innitial commit

---
+++ weatherAPICall.py
@@ -0,0 +1,9 @@
+import requests
+
+# this is the API key that I use to access the weather API  -  https://openweathermap.org/
+API_KEY = "1ba06f056aec6fbe252352ae07e9e7f4"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather" # we send the rquest to this site
+
+
+
+
