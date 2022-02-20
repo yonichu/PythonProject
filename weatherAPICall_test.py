984cedaf1dcc1a8120ef2ff03725c149e100e14e
yoni storaro <73361803+yonichu@users.noreply.github.com>
2022-02-20 19:25:32 +0000

    uploaded wrong file: RE: added 2 more tests that test if the API is working properly

--- weatherAPICall_test.py
+++ weatherAPICall_test.py
@@ -9,3 +9,13 @@ def test_get_status_code_200_for_London_city():
     response = requests.get(f"{BASE_URL}?appid={API_KEY}&q=London")
     assert response.status_code == 200
 
+# test to check if the response we receive is a json object
+def test_check_content_type_equals_json():
+     response = requests.get(f"{BASE_URL}?appid={API_KEY}&q=London")
+     assert response.headers["Content-Type"] == "application/json"
+
+# test to chech if the function correctly retrieves the name of the city we give it
+def test_get_name_of_city_London():
+     response = requests.get(f"{BASE_URL}?appid={API_KEY}&q=London")
+     response_body = response.json()
+     assert response_body["name"] == "London"
